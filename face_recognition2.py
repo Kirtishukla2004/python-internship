9b67fa819192f549c67da36b2fa65f277ecca564
kirti <shuklakirti2004@gmail.com>
2023-10-20 15:15:40 +0530

    changes

--- face_recognition2.py
+++ face_recognition2.py
@@ -1,40 +1,30 @@
-
 import cv2
-import numpy as np
 import os
- 
-def TakeImages():        
 
-    i=0
-    if(i==0):
-        cam = cv2.VideoCapture(0)
-        face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-        #harcascadePath = "haarcascade_frontalface_default.xml"
-        #detector=cv2.CascadeClassifier(harcascadePath)
-        sampleNum=0
-        while(True):
-            ret, img = cam.read()
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            faces = face_classifier.detectMultiScale(gray,1.3,5)
+def TakeImages():
+    cam = cv2.VideoCapture(0)
+    face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    sampleNum = 0
+    
+    while True:
+        ret, img = cam.read()
+        faces = face_classifier.detectMultiScale(img, 1.3, 5)
 
-            for (x,y,w,h) in faces:
-                cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)        
-                #incrementing sample number 
-                sampleNum=sampleNum+1
-                #saving the captured face in the dataset folder TrainingImage
-                cv2.imwrite("camimages\ "+str(sampleNum) + ".jpg", gray[y:y+h,x:x+w])
-                #display the frame
-                cv2.imshow('frame',img)
-            #wait for 100 miliseconds 
-            if cv2.waitKey(100) & 0xFF == ord('q'):
-                break
-            # break if the sample number is morethan 100
-            elif sampleNum>3:
-                break
-        cam.release()
-        cv2.destroyAllWindows() 
-        
-    else:
-        print("no")
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+        cv2.imshow('frame', img)
+        key = cv2.waitKey(1) & 0xFF
+
+        if key == ord('q'):  
+            break
+        elif key == ord('c'):  
+            sampleNum += 1
+            img_name = f"camimages/{sampleNum}.jpg"
+            cv2.imwrite(img_name, img)
+            print(f"Image {sampleNum} captured and saved as {img_name}.")
+
+    cam.release()
+    cv2.destroyAllWindows()
 
 TakeImages()
--- facerecorgination.py
+++ facerecorgination.py
@@ -1,33 +1,50 @@
-import numpy as np
 import cv2
 import os
-import os
+from tkinter import *
+from PIL import Image, ImageTk
 
+def TakeImages():
+    cam = cv2.VideoCapture(0)
+    face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    sampleNum = 0
 
-def takeimages():
-    i = 0
-    if (i == 0):
-        cam = cv2.VideoCapture(0)
-        face_classifier = cv2.CascadeClassifier(
-            'haarcascade_frontalface_default.xml')
-        samplenum = 0
-        while (True):
-            ret, img = cam.read()
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            faces = face_classifier.detectMultiScale(gray, 1.3, 5)
-            for (x, y, w, h) in faces:
-                cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-                samplenum = samplenum+1
-                cv2.imwrite("camimages\ "+ str(samplenum)+"jpg", gray[y:y+h, x:x+w])
-                cv2.imshow("frame", img)
-                if cv2.waitKey(100) & 0xFF == ord('q'):
-                    break
-                elif samplenum > 60:
-                    break
-        cam.realease()
-        cv2.destryAllWindows()
-    else:
-        print("no")
+    while True:
+        ret, img = cam.read()
+        faces = face_classifier.detectMultiScale(img, 1.3, 5)
 
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-takeimages()
+        cv2.imshow('frame', img)
+        key = cv2.waitKey(1) & 0xFF
+
+        if key == ord('q'):  # Press 'q' to quit
+            break
+        elif key == ord('c'):  # Press 'c' to capture an image
+            sampleNum += 1
+            img_name = f"camimages/{sampleNum}.jpg"
+            cv2.imwrite(img_name, img)
+            print(f"Image {sampleNum} captured and saved as {img_name}.")
+
+            # Display the saved image in a Tkinter window
+            display_saved_image(img_name)
+
+    cam.release()
+    cv2.destroyAllWindows()
+
+def display_saved_image(image_path):
+    root = Tk()
+    root.title("Saved Image")
+
+    # Load the image using PIL
+    img = Image.open(image_path)
+    img = img.resize((400, 400), Image.ANTIALIAS)
+    photo = ImageTk.PhotoImage(img)
+
+    # Create a label to display the image
+    label = Label(root, image=photo)
+    label.pack()
+
+    root.mainloop()
+
+TakeImages()
